5b6c9190c3ba0b27c59af9e4b9d76151740f1636
ShtHappens796 <tasos@sht.gr>
2018-12-01 19:31:44 +0200

    Prepare for short url displaying

--- includes/pages/home.php
+++ includes/pages/home.php
@@ -1,5 +1,7 @@
 <form id="shorten">
     <h6>SHOR10.ME</h6>
-    <input name="long">
-    <button type="submit">SHORTEN</button>
+    <!-- <input name="long"> -->
+    <input id="result" class="">asd</input>
+    <!-- <button type="submit">SHORTEN</button> -->
+    <button id="copy-button" class="" type="button">COPY</button>
 </form>
